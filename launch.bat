87797fadf3bc7cd957c263628fdc03ef3203b27e
shominalexander <40673785+shominalexander@users.noreply.github.com>
2022-10-15 13:22:18 +0300

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,31 @@
+echo on
+
+cargo build
+
+pause
+
+start "one" cargo run -- "one"
+
+pause
+
+start "two" cargo run -- "two"
+
+pause
+
+start "three" cargo run -- "three"
+
+pause
+
+start "four" cargo run -- "four"
+
+pause
+
+start "five" cargo run -- "five"
+
+pause
+
+del Cargo.lock
+
+rmdir /s/q target
+
+pause
